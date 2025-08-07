6b13a3fd634bcf08d4c7b09891a7d088039b2146
ranjanhivoco <ranjan@hivoco.com>
2025-08-07 18:34:28 +0530

    add meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { SessionProvider } from "./context/SessionContext";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider>
+      <Head>
+        <title>Artistry Skin Nutrition</title>
+        <meta name="description" content="Gaming Quiz for Amway ABOs" />
+      </Head>
+
       <main className={`inter.className `}>
         <Component {...pageProps} />
       </main>
